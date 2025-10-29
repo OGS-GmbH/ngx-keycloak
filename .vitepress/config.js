2775c1243f19e1e3d15fe7c1207087f080dadaa2
Ian Wenneckers <ian.wenneckers@ogs.de>
2025-10-29 16:54:14 +0100

    docs(vitepress-config): change base

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,8 +1,6 @@
 export default {
-  base: "/doc-test/",
-  title: 'ogs-gmbh/doc-test',
+  base: "/ngx-keycloak/",
+  title: 'ogs-gmbh/ngx-keycloak',
   srcDir: "typedoc/out",
-  ignoreDeadLinks: true,
-  head: [ [ 'link', { rel: 'icon', href: 'https://www.ogs.de/favicon.ico', type: 'image/x-icon' } ] ],
   themeConfig: {}
 };
